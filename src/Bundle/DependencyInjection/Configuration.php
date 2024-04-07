b3def75e0703648fd8edca703e213ecf891558c9
Joshua Behrens <code@joshua-behrens.de>
2024-04-07 03:57:03 +0200

    Add Symfony 7.0 compatibility

--- src/Bundle/DependencyInjection/Configuration.php
+++ src/Bundle/DependencyInjection/Configuration.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('sourceability_instrumentation');
         $rootNode = $treeBuilder->getRootNode();
